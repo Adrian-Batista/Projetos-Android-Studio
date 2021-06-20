975bed6ec3757f6f3f3bf67a38c57ee0f07960d0
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-06-20 18:41:22 -0300

    Revert "Update"
    This reverts commit 7387b0eef78b71865cde0068e399190d93304a3f.

---
+++ Meus Projetos/NavegarEntreActivitys/app/src/main/java/com/example/navegarentreactivitys/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.navegarentreactivitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val message = intent.getStringExtra("MESSAGE")
+        val textView = findViewById<TextView>(R.id.secondActivityTextView)
+        textView.setText(message)
+    }
+}
